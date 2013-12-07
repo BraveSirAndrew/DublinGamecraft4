ecf4d8ca7789a264bd12135b0de981a938a1267d
Andrea <roundcrisis@gmail.com>
2013-12-07 09:58:33 +0000

    First commit

---
+++ Source/Code/EditorPlugin/EditorPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DualityEditor;
+
+namespace DublinGamecraft4.Editor
+{
+	/// <summary>
+	/// Defines a Duality editor plugin.
+	/// </summary>
+    public class DublinGamecraft4EditorPlugin : EditorPlugin
+	{
+		public override string Id
+		{
+			get { return "DublinGamecraft4EditorPlugin"; }
+		}
+	}
+}
